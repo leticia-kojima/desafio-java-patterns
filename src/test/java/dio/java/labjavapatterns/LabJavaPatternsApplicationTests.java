aee8dfe7b8e1a2d945216977bab1e55f63090bbf
leticia-kojima <leticia.missaki@hotmail.com>
2023-10-06 22:17:05 -0300

    Primeiro Commit

---
+++ src/test/java/dio/java/labjavapatterns/LabJavaPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.java.labjavapatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabJavaPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
